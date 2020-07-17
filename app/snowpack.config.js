bd93bea0f98ca11b5e4b2bf9988bf3d2895609eb
Duc Nghiem-Xuan <git@ducnx.com>
2020-07-17 15:43:25 +0900

    feat: use webpack to bundle

--- app/snowpack.config.js
+++ app/snowpack.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   extends: '@snowpack/app-scripts-react',
-  install: [
-    "heroicon/solid/*",
-    "heroicon/outline/*"
-  ],
+  install: ['heroicon/solid/*', 'heroicon/outline/*'],
   scripts: {
     'build:css': 'postcss',
+    'bundle:*': '@snowpack/plugin-webpack',
   },
-  plugins: [],
+  plugins: [['@snowpack/plugin-webpack', { sourceMap: true }]],
 };
