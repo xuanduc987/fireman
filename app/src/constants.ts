7cc1f93e86007a8ea8d7fa4d152f89255fe98eef
Duc Nghiem-Xuan <git@ducnx.com>
2020-07-17 18:31:29 +0900

    feat: allow to config SERVER_URL with ENV

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL =
+  import.meta.env.SNOWPACK_PUBLIC_SRV_URL || 'http://localhost:5000';
 export const GRAPHQL_URL = SERVER_URL + '/graphql';
